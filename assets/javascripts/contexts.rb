fa55d1a5ac189157a4483b1fa064bdee5ded87a2
Martin Becker <mbeckerwork@gmail.com>
2015-12-14 18:16:13 +0000

    changes DSL 'element' to 'component'

--- assets/javascripts/contexts.rb
+++ assets/javascripts/contexts.rb
@@ -1,32 +1,32 @@
 Ruta::Context.define :main do
-  element :header do
+  component :header do
     Header
   end
 
   sub_context :info, :info_view
 
-  element :footer do
+  component :footer do
     Footer
   end
 end
 
 
 Ruta::Context.define :info_view do
-  element :hero do
+  component :hero do
     Hero_Image
   end
 
-  element :scroller do
+  component :scroller do
     Info_Scroller
   end
 
-  element :buttons do
+  component :buttons do
     Buttons
   end
 end
 
 Ruta::Context.define :sign_up do
-  element :sign_up_form do
+  component :sign_up_form do
     Sign_Up_Form
   end
 end
